61212676b2428be2a1a1157457bb34327e832ef1
EJ Nellie <enellie01@gmail.com>
2016-09-12 19:11:24 -0500

    hound

--- app/scripts/minmax/dimMinMax.controller.js
+++ app/scripts/minmax/dimMinMax.controller.js
@@ -416,7 +416,7 @@
                               name: 'Strength'
                             }
                           },
-                          setHash: 0,
+                          setHash: 0
                         };
                         if (validSet(set.armor)) {
                           set.setHash = genSetHash(set.armor);
